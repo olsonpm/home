6871bd6eb1b4b3cf981f023e66b6b6a9e3ab8252
Phil Olson <olsonpm@users.noreply.github.com>
2026-01-08 08:43:49 -0600

    remove sqlite-to-rest link
    it used a hash link which only scrolls down to the project if it's not
    already at the bottom - meaning a lot of the time the link feels dead.

--- app/sections/side-projects/details/beer-kb.js
+++ app/sections/side-projects/details/beer-kb.js
@@ -1,16 +1,13 @@
 import { nbsp } from '@/utils'
 import ProjectLinks from '../project-links'
 
-const SQLiteToRest = () => <a href="#sqlite-to-rest">sqlite-to-rest</a>
-
 const BeerKB = () => (
   <>
     <ProjectLinks github="/olsonpm/beerkb" website="beerkb.philipolsonm.com" />
     <h3>What is it?</h3>
     <p>
       {`An interactive website allowing you to modify beer data stored in a `}
-      <SQLiteToRest />
-      &nbsp;instance.
+      SQLite To Rest &nbsp;instance.
     </p>
 
     <h3>Why create it?</h3>
